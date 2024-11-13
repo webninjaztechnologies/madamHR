9fc5c86ec80e172d073c609d9b2c312e270f1291
Ubuntu <ubuntu@ip-172-31-4-26.ap-south-1.compute.internal>
2024-11-13 12:37:53 +0000

    database

--- horilla/settings.py
+++ horilla/settings.py
@@ -120,20 +120,28 @@ if env("DATABASE_URL", default=None):
     }
 else:
     DATABASES = {
-        "default": {
-            "ENGINE": env("DB_ENGINE", default="django.db.backends.sqlite3"),
-            "NAME": env(
-                "DB_NAME",
-                default=os.path.join(
-                    BASE_DIR,
-                    "TestDB_Horilla.sqlite3",
-                ),
-            ),
-            "USER": env("DB_USER", default=""),
-            "PASSWORD": env("DB_PASSWORD", default=""),
-            "HOST": env("DB_HOST", default=""),
-            "PORT": env("DB_PORT", default=""),
-        }
+       'default': {
+         'ENGINE': 'django.db.backends.postgresql',
+         'NAME': 'for_madamhr',
+         'USER': 'madamhr',
+         'PASSWORD': '12345678',
+         'HOST': 'localhost',
+         'PORT': '5432',
+    	}
+       # "default": {
+           # "ENGINE": env("DB_ENGINE", default="django.db.backends.sqlite3"),
+        #    "NAME": env(
+         #       "DB_NAME",
+          #      default=os.path.join(
+           #         BASE_DIR,
+            #        "TestDB_Horilla.sqlite3",
+             #   ),
+           # ),
+           # "USER": env("DB_USER", default=""),
+           # "PASSWORD": env("DB_PASSWORD", default=""),
+           # "HOST": env("DB_HOST", default=""),
+            #"PORT": env("DB_PORT", default=""),
+        #}
     }
 
 # Password validation
